d1a799c3fb90553bcd64653ed730ff3f1ac8b6c6
Yeray Catalá <yeray.cv@protonmail.com>
2022-10-08 17:25:01 +0200

    Fix add icon full

--- src/components/icons/add.js
+++ src/components/icons/add.js
@@ -2,6 +2,7 @@ export function AddIconOutlined() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
+      role="img"
       fill="none"
       viewBox="0 0 24 24"
       strokeWidth={1.5}
@@ -21,16 +22,15 @@ export function AddIconFull() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      fill="none"
+      role="img"
       viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
+      fill="currentColor"
       className="w-8 mr-6 text-black-light cursor-pointer"
     >
       <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+        fillRule="evenodd"
+        d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 9a.75.75 0 00-1.5 0v2.25H9a.75.75 0 000 1.5h2.25V15a.75.75 0 001.5 0v-2.25H15a.75.75 0 000-1.5h-2.25V9z"
+        clipRule="evenodd"
       />
     </svg>
   );
